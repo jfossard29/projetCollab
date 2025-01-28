5c2d6c2bfaaaaf3fbe0f502d70d281902d955bf5
jfossard29 <j.fossard@hotmail.com>
2025-01-28 11:32:24 +0100

    Ajout de la page discord avec son style

--- serveur/serveur_http.js
+++ serveur/serveur_http.js
@@ -1,14 +1,15 @@
-const express = require('express') // chargement du module express
-const config = require("./config.js") // chargement de la configuration
-const util = require('util')
+const express = require('express'); // chargement du module express
+const config = require("./config.js"); // chargement de la configuration
+const util = require('util');
 
-const base = config.base
-const port_http = config.port_http
+const base = config.base;
+const port_http = config.port_http;
 
 var app = express();
-app.use("/", express.static(base+"/html", { index: 'index.html' }))
-app.use(express.urlencoded({ extended: true }))
+app.use("/", express.static(base + "/html", { index: 'index.html' }));
+app.use("/style", express.static(base + "/style"));
+app.use(express.urlencoded({ extended: true }));
 
 var server = app.listen(port_http, function () {
-console.log('Express server listening on port ' + port_http)
+    console.log('Express server listening on port ' + port_http);
 });
